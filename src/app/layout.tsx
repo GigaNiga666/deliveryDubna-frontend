9773fab556b8304a2e69dccc9aace269714e2914
Admin <stasikbygaev@gmail.com>
2023-12-20 20:34:15 +0500

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import {QueryClient, QueryClientProvider} from "react-query";
 import {useEffect} from "react";
 import Head from "next/head";
 import {IWebApp} from "@/components/types/IWebApp";
-import {init} from "@/components/hooks/useTelegram"
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
 
