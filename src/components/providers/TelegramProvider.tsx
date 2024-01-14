073130729ec0a04458cbd04d1a0a85cea8f45b40
Admin <stasikbygaev@gmail.com>
2024-01-14 22:43:00 +0500

    fix

--- src/components/providers/TelegramProvider.tsx
+++ src/components/providers/TelegramProvider.tsx
@@ -21,6 +21,7 @@ const TelegramProvider : FC<PropsWithChildren> = ({children}) => {
 
         function click() {
             router.replace("/cart")
+            app.showAlert("Test2")
             app.MainButton.offClick(click)
         }
 
